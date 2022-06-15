49a3403d0f5986f6c882e8a7b5c53762a107247d
moritou <sennka1038013@gmail.com>
2022-06-15 10:01:09 +0900

    変更

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-//初期化処理(すべてのピースをランダムに配置する)
-for (let n = 1; n <= 16; n = n + 1) {
-    const piece = document.querySelector('.pos-' + n);
-  
-    // MEMO: 1 ～ 16 でランダムに数値を決めると、番号が被る場合がある
-    piece.style.order = parseInt(Math.random() * 16) + 1;
-  }
+// State 相当の値を準備 (今回の場合は配列)
+// ----------------------------------------------------------------------------
+var peaces = [
+  6,  4,  3, 10,
+  7,  1,  2,  5,
+  9, 13, 12,  8,
+ 15, 14, 11, 16,
+];
 
-  //ピースがクリックしたときに実行する処理(関数)
-function pieceClikHandler(event){
-    console.log('ピースがクリックされました');
-    console.log(event.target);
+
+// Component 相当の関数を準備 (State => View にあたるもの)
+// ----------------------------------------------------------------------------
+function component() {
+ for (let n = 0; n < 16; n = n + 1) {
+   const piece = document.querySelector('.pos-' + (n + 1));
+ 
+   piece.style.order = peaces[n];
+ }
 }
 
-for (let p = 1; p<= 16; p = p + 1) {
-const piece01 = document.querySelector('.pos-'+p);
 
-piece01.addEventListener('click',pieceClikHandler);
+// 初期化処理 (WIP: 全てのピースをランダムに配置する)
+// ----------------------------------------------------------------------------
+component();
+
+// ピースがクリックされたときに実行する処理 (関数)
+// ----------------------------------------------------------------------------
+function pieceClickHandler(event) {
+  console.log('ピースがクリックされました');
+  console.log(event.target);
+}
+
+
+// 1 ～ 15 番ピースのクリックを監視し、クリックされたら pieceClickHandler を呼ぶ
+// ----------------------------------------------------------------------------
+for (let n = 1; n <= 15; n = n + 1) {
+  const piece = document.querySelector('.pos-' + n);
+
+  piece.addEventListener('click', pieceClickHandler);
 }
